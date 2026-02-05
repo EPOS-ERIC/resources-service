178fc25e272d4d59508bc22f349f2debb19dd0a9
Valerio Vinciarelli <valerio.vinciarelli@epos-eric.eu>
2026-02-06 00:03:24 +0100

    update some changes

--- src/main/java/org/epos/api/core/DataServiceProviderGenerationSQL.java
+++ src/main/java/org/epos/api/core/DataServiceProviderGenerationSQL.java
@@ -94,13 +94,26 @@ public class DataServiceProviderGenerationSQL {
         sql.append("  LEFT JOIN metadata_catalogue.address a ON t.address_id = a.instance_id ");
         sql.append("), ");
 
+        // Check which target orgs have NO memberOf (are top-level organizations)
+        // Only top-level orgs should have related organizations listed
+        sql.append("top_level_orgs AS ( ");
+        sql.append("  SELECT t.instance_id ");
+        sql.append("  FROM target_orgs t ");
+        sql.append("  WHERE NOT EXISTS ( ");
+        sql.append("    SELECT 1 FROM metadata_catalogue.organization_memberof om ");
+        sql.append("    WHERE om.organization1_instance_id = t.instance_id ");
+        sql.append("  ) ");
+        sql.append("), ");
+
         // Get related organizations (organizations that are member_of target orgs)
         // organization_memberof table: organization1_instance_id is the member, organization2_instance_id is the parent
+        // Only include related orgs for top-level organizations (matching JPA behavior)
         sql.append("related_orgs AS ( ");
         sql.append("  SELECT om.organization1_instance_id, t.instance_id AS parent_instance_id, ");
         sql.append("         o.legalname AS related_legalname, o.url AS related_url, ");
         sql.append("         o.instance_id AS related_instance_id, a.country AS related_country ");
         sql.append("  FROM target_orgs t ");
+        sql.append("  JOIN top_level_orgs tlo ON t.instance_id = tlo.instance_id ");
         sql.append("  JOIN metadata_catalogue.organization_memberof om ON om.organization2_instance_id = t.instance_id ");
         sql.append("  JOIN metadata_catalogue.organization o ON om.organization1_instance_id = o.instance_id ");
         sql.append("  LEFT JOIN metadata_catalogue.address a ON o.address_id = a.instance_id ");
